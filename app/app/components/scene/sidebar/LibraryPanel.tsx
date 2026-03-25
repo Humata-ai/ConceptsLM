318fe5aafda55f25f5d4a71b1552bbe0cec58ed6
Dan Rasmuson <danrasmuson@gmail.com>
2026-03-25 08:39:37 -0500

    small wording change

--- app/app/components/scene/sidebar/LibraryPanel.tsx
+++ app/app/components/scene/sidebar/LibraryPanel.tsx
@@ -6,7 +6,7 @@ import CollapsibleSection from './CollapsibleSection'
 export default function LibraryPanel() {
   return (
     <SidebarPanel title="Library">
-      <CollapsibleSection title="Manner Verbs">
+      <CollapsibleSection title="Dictionary">
         <div className="p-3 rounded-lg bg-white border border-gray-300 hover:bg-gray-50 cursor-pointer transition-colors">
           <h3 className="font-medium">Follow</h3>
         </div>
