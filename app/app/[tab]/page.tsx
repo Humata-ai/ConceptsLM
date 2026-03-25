67cd81f7b651788a8edad2e24387fd6e6bf26f7c
Dan Rasmuson <danrasmuson@gmail.com>
2026-03-24 20:26:35 -0500

    Changed to library

--- app/app/[tab]/page.tsx
+++ app/app/[tab]/page.tsx
@@ -3,7 +3,7 @@
 import { notFound } from 'next/navigation'
 import { use } from 'react'
 
-const VALID_TABS = ['scene', 'timeline', 'dictionary', 'import-export'] as const
+const VALID_TABS = ['scene', 'timeline', 'library', 'import-export'] as const
 
 export default function TabPage({ params }: { params: Promise<{ tab: string }> }) {
   const { tab } = use(params)
